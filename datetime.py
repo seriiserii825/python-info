a6af5bb946a0a2a53b70da28fe8a26d674363d71
serii <seriiburduja@gmail.com>
2024-02-07 09:52:33 +0200

    feat: file.py

---
+++ datetime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from datetime import datetime
+import os
+import pytz
+
+timeZ_At = pytz.timezone('Europe/Kiev')
+
+# Get the current time in UTC
+current_time_utc = datetime.now(timeZ_At)
+
+print("Current time in UTC:", current_time_utc)
+
+def convert_date(timestamp):
+    d = datetime.utcfromtimestamp(timestamp)
+    formated_date = d.strftime('%d %b %Y - %H:%M:%S')
+    return formated_date
+
+def get_files():
+    dir_downloads = os.path.expanduser('~/Downloads')
+    dir_entries = os.scandir(dir_downloads)
+    for entry in dir_entries:
+        if entry.is_file():
+            info = entry.stat()
+            print(f'{entry.name}\t Last Modified: {convert_date(info.st_mtime)}')
+
+get_files()
---
+++ file.py
@@ -0,0 +1,89 @@
+import os
+# check if doesn't exists file with name 'front-page.php'
+if not os.path.exists('front-page.php'):
+    print('File does not exist')
+
+# create file with name 'front-page.php'
+os.system('touch front-page.php')
+
+# change dir
+os.chdir("../../plugins")
+
+# get current dir
+downloads_dir = os.path.expanduser("~/Downloads")
+
+# Check if a path exists
+os.path.exists("/path/to/directory")
+
+# List files in a directory
+os.listdir("/path/to/directory")
+
+# Join paths
+os.path.join("/path/to", "directory")
+
+# Get the current working directory
+os.getcwd()
+
+# Change directory
+os.chdir("/path/to/new/directory")
+
+# Check if a path is a file or directory
+os.path.isfile("/path/to/file")
+os.path.isdir("/path/to/directory")
+
+# Get the basename of a path
+os.path.basename("/path/to/file")
+
+# Get the directory name of a path
+os.path.dirname("/path/to/file")
+
+# Split the path into its directory and basename
+os.path.split("/path/to/file")
+
+# Get the file info
+download_dir = os.path.expanduser('~/Downloads')
+with os.scandir(download_dir) as entries:
+    for entry in entries:
+        print(entry)
+        print(entry.path)
+        print(entry.stat())
+        print(entry.name)
+
+# List all just files in a directory using os.listdir
+basepath = 'my_directory/'
+for entry in os.listdir(basepath):
+    if os.path.isfile(os.path.join(basepath, entry)):
+        print(entry)
+
+# List all files in a directory using scandir()
+basepath = 'my_directory/'
+with os.scandir(basepath) as entries:
+    for entry in entries:
+        if entry.is_file():
+            print(entry.name)
+
+# List all subdirectories using scandir()
+basepath = 'my_directory/'
+with os.scandir(basepath) as entries:
+    for entry in entries:
+        if entry.is_dir():
+            print(entry.name)
+
+# make dir
+os.mkdir('example_directory/')
+
+import shutil
+
+# Copy a file
+shutil.copy("/source/file", "/destination/file")
+
+# Move a file
+shutil.move("/source/file", "/destination/file")
+
+# Remove a file or directory
+shutil.rmtree("/path/to/directory")
+
+import glob
+
+# Find all Python files in a directory
+python_files = glob.glob("/path/to/directory/*.py")
