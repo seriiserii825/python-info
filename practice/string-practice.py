c8883ca4d5063039717d2bc8a917a4fd7b072066
serii <seriiburduja@gmail.com>
2024-04-09 11:14:38 +0300

    feat: python-string practice

--- practice/string-practice.py
+++ practice/string-practice.py
@@ -1,19 +1,44 @@
 #!/bin/python3
+from termcolor import colored
+def findSubstr(string, substring):
+    if substring.lower() in string.lower():
+        return True
+    return False
 
-s = input('Write a string: ').strip()
-symbol = input('Input a symbol in string: ').strip()
-count = 0
-symbol_count = s.count(symbol)
+# print(findSubstr('hello', 'ts'))
 
-if symbol_count == 0:
-    print(f"Symbol '{symbol}' is not in the string")
-else:
-    print(f"Symbol '{symbol}' occurs {symbol_count} times in the string")
+def firstLastIndex(string, symbol):
+    first = string.find(symbol)
+    if first == -1:
+        return (None, None)
+    last = string.rfind(symbol)
+    if last == first:
+        return (first, None)
+    return (first, last)
 
-# result = ''
-# for i in range(len(s)):
-#     if i % 2 == 0:
-#         result += s[i]
-# print(result)
+# print("hello", 'll', firstLastIndex('hello', 'll'))
+# print("hello", 'l',firstLastIndex('hello', 'l'))
+# print("hello", 'o',firstLastIndex('hello', 'o'))
+# print("hello", 't',firstLastIndex('hello', 't'))
+
+def top3Str(text):
+    count_res = []
+    words = text.split()
+    if len(words) < 3:
+        print(colored("Not enough words in text, need at least 3 words.", "red"))
+        return
+    words = set(words)
+    words = list(words)
+    for word in words:
+        count_res.append([word, text.count(word)])
+    count_res.sort(key=lambda x: x[1], reverse=True)
+    print(f"""Top 3 words in text: 
+          {count_res[0][0]} - {count_res[0][1]},
+          {count_res[1][0]} - {count_res[1][1]},
+          {count_res[2][0]} - {count_res[2][1]}""")
+
+# top3Str('hello my world')
+
+
 
 
