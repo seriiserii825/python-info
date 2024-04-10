6227cd0939cfae109a146ea3cd42a761ab1d2d62
serii <seriiburduja@gmail.com>
2024-04-10 18:35:59 +0300

    updated by script at 2024-04-10 18:35:28

--- tuple.py
+++ tuple.py
@@ -1,7 +1,17 @@
-t1 = (1, 2, 3)
-t2 = (4, 5, 6)
-t3 = t1 + t2
+x = tuple('hello')
+print(x) # ('h', 'e', 'l', 'l', 'o')
 
-print(t1.index(2))
+# упаковка
+x, y, z = (1, 2, 3)
 
-print(t3)
+# распаковка
+x, y, z = (1, 2, 3)
+print(x) # 1
+print(y) # 2
+print(z) # 3
+
+
+x, y, z, *t = 1, 2, 3, 8, 9
+print(x) # 1
+print(y) # 2
+print(t) # [8, 9]
